a1076d0f47d694e63eb97f87df1492bae3b656ec
aziztaufiqurrahman <taufiqqurrahman2899@gmail.com>
2021-12-20 20:04:32 +0700

    Ngehapus Top Produk

--- riwayat_transaksi.php
+++ riwayat_transaksi.php
@@ -2,7 +2,6 @@
 include "connect.php";
 include "models/transactions.php";
 $dataTransaksi= Transactions::getAll($db);
-print_r (pretty($dataTransaksi));
 ?>
 
 <!DOCTYPE html>
@@ -21,41 +20,26 @@ print_r (pretty($dataTransaksi));
 <thead>
 <tr>
 <th>No</th>
-<th>Tanggal</th>
-<th>Status</th>
+<th>Tanggal Berlangsung</th>
+<th>Kode</th>
 <th>Total</th>
-<th>Opsi</th>
+<th>Aksi</th>
 </tr>
 </thead>
 <tbody>
 <?php
 $nomor=1;
-$id_pelanggan = $_SESSION["pelanggan"] ['id_pelanggan'];
-
-$ambil = $koneksi->query("SELECT * FROM pembelian WHERE id_pelanggan='$id_pelanggan'");
-while ($pecah = $ambil->fetch_assoc()) {
 ?>
 
+<td><?php echo $nomor++; ?></td>
+<?php foreach ($dataTransaksi as $key) {
+echo "<tr>";
+echo "<td>". $key["CREATED_AT"]."</td>";
+echo "<td>". $key["CODE"]. "</td>";
+echo "<td>". "</td>";
+echo "</tr>";
+}?> 
 
-<tr>
-<td><?php echo $nomor; ?></td>
-<td><?php echo $pecah["tanggal_pembelian"] ?></td>
-<td><?php echo $pecah["status_pembelian"] ?>
-<br>
-<?php if (!empty($pecah['resi_pengiriman'])): ?> RESI : <?php echo $pecah['resi_pengiriman']?>
-<?php endif ?>
-
-</td>
-<td><?php echo number_format($pecah["total_pembelian"]) ?></td>
-<td>
-
-<a href="nota.php?id=<?php echo $pecah["id_pembelian"] ?>" class="btn btn- info">Nota</a>
-<a href="pembayaran.php?id=<?php echo $pecah["id_pembelian"];?>" class="btn btn-success">Pembayaran</a>
-</td>
-</tr>
-<?php $nomor++; ?>
-<?php 
-} ?>
 </tbody>
 </table>
 </div>
