159c41649b952b980ff8cd1b5ba2e7b793327223
aziztaufiqurrahman <taufiqqurrahman2899@gmail.com>
2021-12-21 22:31:12 +0700

    Menghapus komen php di employee.php

--- employee.php
+++ employee.php
@@ -261,20 +261,6 @@ $employee = Employees::getAll($db);
               "processing": true, //Feature control the processing indicator.
               "serverSide": true, //Feature control DataTables' server-side processing mode.
               "order": [], //Initial no order.
-              // Load data for the table's content from an Ajax source
-              // "ajax": {
-              //     "url": '<?php echo site_url('karyawan/json'); ?>',
-              //     "type": "POST"
-              // },
-              //Set column definition initialisation properties.
-              // "columns": [
-              //     {"data": "foto",width:170},
-              //     {"data": "nama_lengkap",width:100},
-              //     {"data": "email",width:100},
-              //     {"data": "no_hp",width:100},
-              //     {"data": "action",width:100}
-              // ],
-
           });
 
       });
