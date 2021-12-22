938ecf6e256b293afa066ace1804f54dff631250
Zlarex <ihsanfauzan76@gmail.com>
2021-12-22 17:28:24 +0700

    get all data

--- models/products.php
+++ models/products.php
@@ -4,7 +4,17 @@ class Products
 {
     public static function getAll ($db) 
     {
-        $products = "SELECT pr.*, pc.price FROM products pr join prices pc on pc.id_product = pr.id_product";
+        $products = "SELECT pr.id_product, pr.name, pr.unit, pi.price, pi.created_at AS price_at, SUM(ba.stock) AS stock
+        FROM products pr
+        JOIN prices pi ON pi.id_product = pr.id_product
+        JOIN batches ba ON ba.id_product = pr.id_product
+        WHERE
+            pr.id_product = 1 AND
+            TO_NUMBER(TO_CHAR(expired_at, 'YYYYMMDDHH24MISS')) - TO_NUMBER(TO_CHAR((CURRENT_TIMESTAMP + INTERVAL '3' DAY), 'YYYYMMDDHH24MISS')) <> 0 AND
+            pr.deleted_at IS NULL
+        GROUP BY pr.id_product, pr.name, pr.unit, pi.price, pi.created_at
+        ORDER BY pi.created_at DESC
+        FETCH FIRST 1 ROWS ONLY;";
         $stmt = $db->prepare($products);
         $stmt->execute();
         $temp = $stmt->fetchAll ();
