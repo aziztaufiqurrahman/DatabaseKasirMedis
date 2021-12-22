d090cc506cc204e6d644431c49403f6a39cb3aa8
Zlarex <ihsanfauzan76@gmail.com>
2021-12-22 17:51:57 +0700

    for add stock product

--- models/products.php
+++ models/products.php
@@ -2,12 +2,24 @@
 
 class Products
 {
+    /**
+     * mendapatkan data semua product
+     */
     public static function getAll ($db) 
     {
-        $products = "SELECT pr.id_product, pr.name, pr.unit, pi.price, pi.created_at AS price_at, SUM(ba.stock) AS stock FROM products pr JOIN prices pi ON pi.id_product = pr.id_product JOIN batches ba ON ba.id_product = pr.id_product WHERE TO_NUMBER(TO_CHAR(expired_at, 'YYYYMMDDHH24MISS')) - TO_NUMBER(TO_CHAR((CURRENT_TIMESTAMP + INTERVAL '3' DAY), 'YYYYMMDDHH24MISS')) <> 0 AND pr.deleted_at IS NULL GROUP BY pr.id_product, pr.name, pr.unit, pi.price, pi.created_at ORDER BY pi.created_at DESC FETCH FIRST 1 ROWS ONLY";
+        $products = "SELECT pr.id_product, pr.name, pr.unit, pi.price, pi.created_at AS price_at, SUM(ba.stock) AS stock FROM products pr JOIN prices pi ON pi.id_product = pr.id_product JOIN batches ba ON ba.id_product = pr.id_product WHERE TO_NUMBER(TO_CHAR(expired_at, 'YYYYMMDDHH24MISS')) - TO_NUMBER(TO_CHAR((CURRENT_TIMESTAMP + INTERVAL '3' DAY), 'YYYYMMDDHH24MISS')) <> 0 AND pr.deleted_at IS NULL GROUP BY pr.id_product, pr.name, pr.unit, pi.price, pi.created_at ORDER BY pi.created_at DESC";
         $stmt = $db->prepare($products);
         $stmt->execute();
         $temp = $stmt->fetchAll ();
+        return $temp;
+    }
+    public static function getForAddStock($db, $id_product)
+    {
+        $sql = "SELECT id_product, name FROM products WHERE deleted_at IS NULL and id_product = :id_product";
+        $stmt = $db->prepare($products);
+        $stmt->bindValue(":id_product", $id_product);
+        $stmt->execute();
+        $temp = $stmt->fetch();
         return $temp;
     }
 }
