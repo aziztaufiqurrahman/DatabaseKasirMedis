b92a2cf61d6f257d9bc6eea5da21927edd87bc7b
Zlarex <ihsanfauzan76@gmail.com>
2021-12-23 21:57:24 +0700

    view batches

---
+++ models/batches.php
@@ -0,0 +1,20 @@
+<?php
+class Batches
+{
+    public static function view($db, $id_product)
+    {
+        $sql = "SELECT *
+        FROM batches
+        WHERE
+            TO_NUMBER(TO_CHAR(expired_at, 'YYYYMMDDHH24MISS')) - TO_NUMBER(TO_CHAR((CURRENT_TIMESTAMP + INTERVAL '3' DAY), 'YYYYMMDDHH24MISS')) <> 0 AND
+            id_product = :id_product
+        ORDER BY expired_at ASC";
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(":id_product", $id_product);
+        $stmt->execute();
+        $row = $stmt->fetchAll();
+        return $row;
+    }
+}
+
+?>
