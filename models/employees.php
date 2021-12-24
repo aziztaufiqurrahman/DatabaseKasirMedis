3f4a913c494f91c6512a248ef061d4a2a38d68dd
Zlarex <ihsanfauzan76@gmail.com>
2021-12-24 13:54:54 +0700

    method employees

--- models/employees.php
+++ models/employees.php
@@ -14,7 +14,7 @@ class Employees
         return $temp;
     }
     /**
-     * Melakukan login
+     * melakukan login
      */
     public static function login($db, $username, $password)
     {
@@ -25,6 +25,38 @@ class Employees
         $stmt->execute();
         $temp = $stmt->fetch();
         return json_encode($temp, JSON_NUMERIC_CHECK);
+    }
+    /**
+     * tambah employees
+     */
+    public static function insert($db, $username, $password, $name, $role, $phone, $address)
+    {
+        $sql = "CALL insert_employee(:username, standard_hash(:password, 'MD5'), :name, :role, :phone, :address)";
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(":username", $username);
+        $stmt->bindValue(":password", $password);
+        $stmt->bindValue(":name", $name);
+        $stmt->bindValue(":role", $role);
+        $stmt->bindValue(":phone", $phone);
+        $stmt->bindValue(":address", $address);
+        $stmt->execute();
+    }
+    public static function update($db, $id_employee, $password, $name, $address)
+    {
+        $sql = "CALL update_employee(:id_employee, :password, :name, :address)";
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(":id_employee", $id_employee);
+        $stmt->bindValue(":password", $password);
+        $stmt->bindValue(":name", $name);
+        $stmt->bindValue(":address", $address);
+        $stmt->execute();
+    }
+    public function delete($db, $id_employee)
+    {
+        $sql = "CALL softdelete_employee(:id_employee)";
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(":id_employee", $id_employee);
+        $stmt->execute();
     }
 }
 ?>
