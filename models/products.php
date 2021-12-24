aa2d52a815fda0e1f1c37504486cd0d450dae811
Zlarex <ihsanfauzan76@gmail.com>
2021-12-24 21:36:16 +0700

    by type

--- models/products.php
+++ models/products.php
@@ -22,6 +22,25 @@ class Products
         $temp = $stmt->fetchAll ();
         return $temp;
     }
+    public function getAllByType($db, $id_type)
+    {
+        $sql = "SELECT pr.id_product, pr.name, pr.unit, pi.price, pi.created_at AS price_at, SUM(ba.stock) AS stock
+        FROM products pr
+        JOIN prices pi ON pi.id_product = pr.id_product
+        JOIN batches ba ON ba.id_product = pr.id_product
+        WHERE
+            TO_NUMBER(TO_CHAR(expired_at, 'YYYYMMDDHH24MISS')) - TO_NUMBER(TO_CHAR((CURRENT_TIMESTAMP + INTERVAL '3' DAY), 'YYYYMMDDHH24MISS')) <> 0 AND
+            pr.deleted_at IS NULL AND
+            pi.created_at = (SELECT pri.created_at FROM prices pri WHERE pri.id_product = pr.id_product ORDER BY pri.created_at DESC FETCH FIRST 1 ROWS ONLY) AND
+            pr.id_type = :id_type
+        GROUP BY pr.id_product, pr.name, pr.unit, pi.price, pi.created_at
+        ORDER BY pi.created_at DESC";
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(":id_type", $id_type);
+        $stmt->execute();
+        $temp = $stmt->fetchAll ();
+        return $temp;
+    }
     /**
      * ambil id dan nama produk
      */
